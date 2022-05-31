9bee41d4842cb0d439fc4345e40bb296a30e0a9d
Danylo Donets <EndoBroYT@gmail.com>
2022-05-31 14:27:29 +0300

    Add files via upload

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"domain","l":"Animal"},{"p":"domain","l":"Reptile"},{"p":"domain","l":"Snake"},{"p":"test","l":"TestAnimal"}];updateSearchResults();
