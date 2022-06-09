060aa04a17de16096807072bd250e6b07033be0a
Danylo Donets <EndoBroYT@gmail.com>
2022-06-09 15:42:57 +0300

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"domain","l":"Animal"},{"p":"domain","l":"Reptile"},{"p":"domain","l":"Snake"},{"p":"test","l":"TestAnimal"}];updateSearchResults();
